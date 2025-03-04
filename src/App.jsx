c08cf7a0cf35930df22635a258065a61d64dbdd8
Shreycode28 <shrey.patel.s73@kalvium.community>
2025-03-04 14:26:38 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Component from './components/TeamMemberCard'
+
+function App() {
+  return (
+    <>
+      <Component/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/TeamMemberCard.jsx
@@ -0,0 +1,16 @@
+function Component() {
+  return (
+    <>
+      <div>
+        <h1>John Doe</h1>
+        <h2>Software Engineer</h2>
+      </div>
+      <div>
+        <h1>Tom Hardy</h1>
+        <h2>Hardware Engineer</h2>
+      </div>
+    </>
+  )
+}
+
+export default Component;
